f55bf62abfb753aaa5bbe7082ecb3f5755be48f3
Moises Velasquez <mvelasquez@pictoric.com>
2021-02-02 18:27:10 -0500

    alternative syntaxis

--- app/Views/structure/body.php
+++ app/Views/structure/body.php
@@ -20,22 +20,24 @@
                 <th>Deleted</th>
                 <th>Actions</th>
             </tr>
+            <?php if ($users): ?>
             <?php foreach ($users as $user): ?>
             <tr>
-                <td><?php echo $user['id'] ?></td>
-                <td><?php echo $user['name'] ?></td>
-                <td><?php echo $user['email'] ?></td>
-                <td><?php echo $user['deleted_at'] != null ? $user['deleted_at'] : '0'  ?></td>
+                <td><?= $user['id'] ?></td>
+                <td><?= $user['name'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td><?= $user['deleted_at'] != null ? $user['deleted_at'] : '0'  ?></td>
                 <td>
-                    <a class="btn btn-primary" href="<?php echo base_url()?>/HelloWorld/edit/<?php echo $user['id'] ?>">
+                    <a class="btn btn-primary" href="<?= base_url()?>/HelloWorld/edit/<?php echo $user['id'] ?>">
                         <i class="fas fa-edit"></i>
                     </a>
-                    <a class="btn btn-danger" href="<?php echo base_url()?>/HelloWorld/delete/<?php echo $user['id'] ?>">
+                    <a class="btn btn-danger" href="<?= base_url()?>/HelloWorld/delete/<?php echo $user['id'] ?>">
                         <i class="fas fa-trash"></i>
                     </a>
                 </td>
             </tr>
             <?php endforeach?>
+            <?php endif ?>
         </table>
     </main>
 </body>
